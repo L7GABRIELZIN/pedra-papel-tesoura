0b87330da901c330a05560876591d016ab777728
L7GABRIELZIN <165018281+L7GABRIELZIN@users.noreply.github.com>
2024-04-02 08:02:25 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -3,12 +3,14 @@ input.onGesture(Gesture.Shake, function () {
     x = randint(0, 2)
     if (x == 0) {
         basic.showIcon(IconNames.SmallSquare)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Punchline), music.PlaybackMode.InBackground)
     } else if (x == 1) {
         basic.showIcon(IconNames.Square)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Ode), music.PlaybackMode.InBackground)
     } else {
         basic.showIcon(IconNames.Scissors)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
     }
-    music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
 })
 basic.forever(function () {
 	
